91413c5bb4c275feadbef5385dc9d25fb7468ea7
Ishan Goyal <63078216+Ishan27g@users.noreply.github.com>
2022-03-14 03:34:32 +1100

    wtf

--- load_test.go
+++ load_test.go
@@ -4,19 +4,24 @@
 package main
 
 import (
+	"context"
 	"testing"
 	"time"
 )
 
 const bulkMessages = 100
 
+//func Test_Gossip_Load_Single_Round(t *testing.T) {
+//	ctx, can := context.WithCancel(context.Background())
+//	defer can()
+//	runTest(t, ctx, zone1, bulkMessages, atAny, func() time.Duration {
+//		return time.Millisecond * 100
+//	})
+//}
 func Test_Gossip_Load_Multiple_Rounds(t *testing.T) {
-	runTest(t, zone1, bulkMessages, atAny, func() time.Duration {
+	ctx, can := context.WithCancel(context.Background())
+	defer can()
+	runTest(t, ctx, zone1, bulkMessages, atAny, func() time.Duration {
 		return 1*time.Second + syncDelay
-	})
-}
-func Test_Gossip_Load_Single_Round(t *testing.T) {
-	runTest(t, zone1, bulkMessages, atAny, func() time.Duration {
-		return time.Millisecond * 100
 	})
 }
